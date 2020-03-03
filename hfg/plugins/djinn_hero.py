750d4fd725697f84afb87d0482f43372484f6ca3
JakobJBauer <private@jakobjbauer.com>
2020-03-03 12:13:21 +0100

    Added block and body and changed behaviour

--- hfg/plugins/djinn_hero.py
+++ hfg/plugins/djinn_hero.py
@@ -5,17 +5,20 @@ import time
 import sys
 import os
 import numpy
+import math
+from threading import Thread
 
 sys.path.append("..")
 
 from plugins import Hero, Enemy, plugin
 from base import threaded
 from shapes import *
+from drawing import ReferenceFrame
 
 @plugin
 class DjinnHero(Hero):
     def __init__(self, resources_path: str = __file__):
-        super().__init__(width=60, height=70, name="djinn", title="Djinn", special_color=pyxel.COLOR_LIME)
+        super().__init__(width=60, height=70, health=80, special=80, name="djinn", title="Djinn", special_color=pyxel.COLOR_LIME)
         self.is_blocked = False
         # self.rect = Rectangle(0, 0, self.width, self.height, pyxel.COLOR_GREEN, self.frame)
         #  self.sprinkles = Sprinkles(100, pyxel.COLOR_BROWN, self.rect)
@@ -151,29 +154,66 @@ class DjinnHero(Hero):
         pass
 
     def _body(self):
-        self.draw(Circle(37, 60, 7, pyxel.COLOR_NAVY, self.frame))
-        self.draw(Circle(35, 58, 6, pyxel.COLOR_NAVY, self.frame))
-        self.draw(Circle(34, 51, 9, pyxel.COLOR_NAVY, self.frame))
-        self.draw(Rectangle(18, 45, 30, 5, pyxel.COLOR_NAVY, self.frame))
+        colors = [
+            pyxel.COLOR_NAVY,
+            pyxel.COLOR_RED,
+            pyxel.COLOR_BLACK,
+            pyxel.COLOR_DARKGRAY,
+            pyxel.COLOR_WHITE,
+            pyxel.COLOR_RED,
+            pyxel.COLOR_PINK,
+            pyxel.COLOR_ORANGE,
+            pyxel.COLOR_CYAN
 
+        ]
+
+        bitmap = [
+            [2, 2, 1, 0, 0, 8, 8, 1, 2, 2, 2, 2, 2, 2, 2],
+            [2, 1, 0, 1, 8, 8, 8, 8, 1, 2, 2, 2, 2, 2, 2],
+            [2, 2, 1, 1, 8, 2, 8, 2, 1, 1, 2, 2, 2, 2, 2],
+            [2, 1, 0, 1, 8, 8, 8, 8, 1, 0, 1, 2, 2, 2, 2],
+            [1, 0, 8, 0, 0, 0, 0, 0, 0, 8, 0, 1, 2, 2, 2],
+            [0, 8, 8, 0, 0, 0, 8, 8, 0, 8, 8, 0, 1, 2, 2],
+            [0, 8, 8, 8, 8, 8, 8, 0, 0, 0, 8, 0, 1, 2, 2],
+            [1, 0, 8, 8, 8, 8, 0, 8, 8, 8, 0, 1, 2, 2, 2],
+            [2, 1, 8, 0, 0, 0, 8, 0, 0, 0, 1, 2, 2, 2, 2],
+            [2, 2, 1, 1, 0, 0, 8, 8, 8, 0, 0, 1, 2, 2, 2],
+            [2, 1, 1, 2, 1, 1, 0, 0, 8, 8, 8, 0, 1, 2, 2],
+            [1, 0, 0, 1, 2, 2, 1, 0, 8, 8, 8, 0, 1, 2, 2],
+            [0, 1, 0, 0, 1, 2, 2, 1, 8, 8, 8, 0, 1, 2, 2],
+            [0, 1, 1, 0, 0, 1, 1, 0, 8, 8, 8, 0, 1, 2, 2],
+            [1, 0, 1, 0, 8, 0, 0, 8, 8, 8, 0, 1, 2, 2, 2],
+            [1, 1, 1, 1, 0, 0, 8, 8, 8, 0, 1, 2, 2, 2, 2]
+        ]
+
+        for y in range(0,64):
+            for x in range(0,60):
+                self.draw(Point(60 - x, 70 - y, colors[bitmap[math.floor(y/4)][math.floor(x/4)]], self.frame))
+
+        if self.is_blocked:
+            self.draw(Line(-6, 5, -6, self.height + 3, pyxel.COLOR_LIME, self.frame))
+
+    def _carpet(self, reference_frame: ReferenceFrame = None):
+        if reference_frame is None:
+            reference_frame = self.frame
         # carpet
-        self.draw((Line(0, 4, 60, 4, pyxel.COLOR_ORANGE, self.frame)))
-        self.draw((Line(0, 5, 4, 5, pyxel.COLOR_ORANGE, self.frame)))
-        self.draw((Line(0, 4, 4, 4, pyxel.COLOR_BLACK, self.frame)))
-        self.draw((Line(8, 3, 12, 3, pyxel.COLOR_ORANGE, self.frame)))
-        self.draw((Line(8, 4, 12, 4, pyxel.COLOR_BLACK, self.frame)))
-        self.draw((Line(16, 5, 20, 5, pyxel.COLOR_ORANGE, self.frame)))
-        self.draw((Line(16, 4, 20, 4, pyxel.COLOR_BLACK, self.frame)))
-        self.draw((Line(24, 3, 28, 3, pyxel.COLOR_ORANGE, self.frame)))
-        self.draw((Line(24, 4, 28, 4, pyxel.COLOR_BLACK, self.frame)))
-        self.draw((Line(32, 5, 36, 5, pyxel.COLOR_ORANGE, self.frame)))
-        self.draw((Line(32, 4, 36, 4, pyxel.COLOR_BLACK, self.frame)))
-        self.draw((Line(40, 3, 44, 3, pyxel.COLOR_ORANGE, self.frame)))
-        self.draw((Line(40, 4, 44, 4, pyxel.COLOR_BLACK, self.frame)))
-        self.draw((Line(48, 5, 52, 5, pyxel.COLOR_ORANGE, self.frame)))
-        self.draw((Line(48, 4, 52, 4, pyxel.COLOR_BLACK, self.frame)))
-        self.draw((Line(56, 3, 60, 3, pyxel.COLOR_ORANGE, self.frame)))
-        self.draw((Line(56, 4, 60, 4, pyxel.COLOR_BLACK, self.frame)))
+        self.draw((Line(0, 4, 60, 4, pyxel.COLOR_ORANGE, reference_frame)))
+        self.draw((Line(0, 5, 4, 5, pyxel.COLOR_ORANGE, reference_frame)))
+        self.draw((Line(0, 4, 4, 4, pyxel.COLOR_BLACK, reference_frame)))
+        self.draw((Line(8, 3, 12, 3, pyxel.COLOR_ORANGE, reference_frame)))
+        self.draw((Line(8, 4, 12, 4, pyxel.COLOR_BLACK, reference_frame)))
+        self.draw((Line(16, 5, 20, 5, pyxel.COLOR_ORANGE, reference_frame)))
+        self.draw((Line(16, 4, 20, 4, pyxel.COLOR_BLACK, reference_frame)))
+        self.draw((Line(24, 3, 28, 3, pyxel.COLOR_ORANGE, reference_frame)))
+        self.draw((Line(24, 4, 28, 4, pyxel.COLOR_BLACK, reference_frame)))
+        self.draw((Line(32, 5, 36, 5, pyxel.COLOR_ORANGE, reference_frame)))
+        self.draw((Line(32, 4, 36, 4, pyxel.COLOR_BLACK, reference_frame)))
+        self.draw((Line(40, 3, 44, 3, pyxel.COLOR_ORANGE, reference_frame)))
+        self.draw((Line(40, 4, 44, 4, pyxel.COLOR_BLACK, reference_frame)))
+        self.draw((Line(48, 5, 52, 5, pyxel.COLOR_ORANGE, reference_frame)))
+        self.draw((Line(48, 4, 52, 4, pyxel.COLOR_BLACK, reference_frame)))
+        self.draw((Line(56, 3, 60, 3, pyxel.COLOR_ORANGE, reference_frame)))
+        self.draw((Line(56, 4, 60, 4, pyxel.COLOR_BLACK, reference_frame)))
 
     def cycle_heads(self, direction):
         pass
@@ -185,9 +225,28 @@ class DjinnHero(Hero):
         pass
 
     def start_animation(self, enemy):
+        self.release()
+        time.sleep(1)
+
+        thread = Thread(target=self._sprinkles, args=[False])
+        thread.start()
+
+        time.sleep(2.2)
+
         self._body()
+        self._carpet()
 
         self.release()
+
+    def _sprinkles(self, show_body=True):
+        for i in range(20):
+            self.draw(Sprinkles(20, pyxel.COLOR_YELLOW, Rectangle(0,0,self.width,self.height,pyxel.COLOR_WHITE, self.frame)))
+            self._carpet()
+            if show_body:
+                self._body()
+            self.draw(Text("**Snap**", 100, 75, pyxel.COLOR_PINK, ReferenceFrame(0, 0)))
+            time.sleep(0.1)
+            self.release()
 
     def attack(self, enemy: Enemy):
         xy: list = list()
@@ -201,6 +260,7 @@ class DjinnHero(Hero):
             current_y = 0
             while current_y < y:
                 self._body()
+                self._carpet()
                 self.draw(CircleBorder(x, current_y + 3, 1, pyxel.COLOR_LIME, self.frame))
                 self.draw(Line(x, current_y + 2, x, current_y, pyxel.COLOR_LIME, self.frame))
                 self.draw(Line(x - 1, current_y, x + 1, current_y, pyxel.COLOR_LIME, self.frame))
@@ -241,6 +301,7 @@ class DjinnHero(Hero):
             while (enemy.x + x) - self.x > -(enemy.width/2) or (self.x + x) - enemy.x > -(enemy.width/2):
                 x -= 1
                 self._body()
+                self._carpet()
 
                 for shape in ready_attacks[0]:
                     if shape[0] == "CircleBorder":
@@ -268,16 +329,18 @@ class DjinnHero(Hero):
                 self.release()
 
             if (ready_attacks[0][2][2] > enemy.y) and (ready_attacks[0][2][2] < enemy.y + enemy.height):
-                enemy.get_attacked(self, 30)
+                enemy.get_attacked(self, 15)
 
             ready_attacks.pop(0)
 
         self._body()
+        self._carpet()
         self.release()
 
     def super(self, enemy: Enemy):
-        for i in range(100):
+        for i in range(250):
             self._body()
+            self._carpet()
 
             time.sleep(0.01)
 
@@ -287,26 +350,74 @@ class DjinnHero(Hero):
             self.draw(Sprinkles(30, pyxel.COLOR_YELLOW, rect))
             self.release()
 
-        self.special -= 20
+        self.special -= 30
         enemy.get_attacked(enemy, 50)
+        self.health += (80-self.health) * 0.5
         self._body()
+        self._carpet()
         self.release()
 
     def block(self, enemy: Enemy):
+        self._body()
+        self._carpet()
+        self.draw(Line(-6, 5, -6, self.height + 3, pyxel.COLOR_LIME, self.frame))
+        self.release()
         self.is_blocked = True
 
     def walk(self, direction, enemy: Enemy):
-        for i in range(8):
-            time.sleep(0.03)
-
-            if i == 0:
-                self.y += 3
-
+        for i in range(12):
+            time.sleep(0.05)
             self.x += direction
 
-        self.y -= 3
+            self._body()
+
+            self.draw(Line(0, 4, 60, 4, pyxel.COLOR_ORANGE, self.frame))
+            self.draw((Line(0, 5, 4 - i, 5, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(0, 4, 4 - i, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(8, 3, 12 - i, 3, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(8, 4, 12 - i, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(16, 5, 20 - i, 5, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(16, 4, 20 - i, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(24, 3, 28 - i, 3, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(24, 4, 28 - i, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(32, 5, 36 - i, 5, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(32, 4, 36 - i, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(40, 3, 44 - i, 3, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(40, 4, 44 - i, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(48, 5, 52 - i, 5, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(48, 4, 52 - i, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(56, 3, 60 - i, 3, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(56, 4, 60 - i, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(64, 5, 68 - i, 5, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(64, 4, 68 - i, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(72, 4, 76 - i, 3, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(72, 4, 76 - i, 4, pyxel.COLOR_ORANGE, self.frame)))
+            self.draw((Line(-7, 3, 0, 3, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(-7, 4, 0, 4, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Line(-7, 5, 0, 5, pyxel.COLOR_BLACK, self.frame)))
+            self.draw((Rectangle(63, 3, 17, 2, pyxel.COLOR_BLACK, self.frame)))
+
+            self.release()
+
+        self._carpet()
+        self._body()
+        self.release()
+
+    def _win_sprinkles(self):
+        fixed_x = self.x
+        fixed_y = self.y
+        for i in range(40):
+            self._body()
+            self._carpet(ReferenceFrame(fixed_x, fixed_y))
+            self.draw(Sprinkles(30, pyxel.COLOR_PINK, Rectangle(-5,-5,70,80,pyxel.COLOR_WHITE,self.frame)))
+            self.release()
+            time.sleep(0.1)
+        self._carpet()
+        self._body()
+        self.release()
 
     def win_animation(self, enemy: Enemy):
+        Thread(target=self._win_sprinkles).start()
         self.move_to(enemy, 5)
         for i in range(10):
             time.sleep(0.03)
@@ -319,9 +430,13 @@ class DjinnHero(Hero):
         time.sleep(2)
 
     def lose_animation(self, enemy: Enemy):
-        for i in range(500):
-            time.sleep(0.01)
+        for i in range(self.height + self.y):
+            self._body()
+            self._carpet()
+            self.draw(Rectangle(0, self.height, self.width, 30, pyxel.COLOR_BLACK, self.frame))
             self.y -= 1
+            time.sleep(0.05)
+            self.release()
 
     def get_attacked(self, enemy, damage):
         if not self.is_blocked:
@@ -330,6 +445,9 @@ class DjinnHero(Hero):
         if self.health < 0:
             self.health = 0
         self.is_blocked = False
+        self._body()
+        self._carpet()
+        self.release()
 
     def get_pushed(self, enemy, distance):
         self.move_to(enemy, distance)
